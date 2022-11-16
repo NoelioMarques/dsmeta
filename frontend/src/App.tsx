9be2cd4e3a6bf366910ec9d65e4f831aff1038f5
Noelio Marques <noeliocz@gmail.com>
2022-11-16 00:29:47 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,15 @@
+import NotificationButton from "./components"
+
 function App() {
   return (
+    <>
     <h1>Olá, Mundo!!</h1>
+    <NotificationButton />
+    <NotificationButton />
+    <NotificationButton />
+    <NotificationButton />
+    <NotificationButton />
+    </>
   )
 }
 
